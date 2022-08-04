5500a64208bcb138564b293f72b12f661eb11fcf
txitxe36 <miguelgarcia_654@hotmail.com>
2022-08-03 21:02:31 -0300

    iniciando commit

---
+++ src/test/java/com/miguelgarcia/alkemyChallenge/AlkemyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelgarcia.alkemyChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
